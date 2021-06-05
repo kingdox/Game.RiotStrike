6f234aa2ba986a7ca06fb7bd26013395b1ba423d
kingdox <arpaxavier@gmail.com>
2021-06-05 02:59:27 +0200

    no message

--- Riot Strike/Assets/Scripts/Bases/Spell/SuperDashSpell.cs
+++ Riot Strike/Assets/Scripts/Bases/Spell/SuperDashSpell.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using XavHelpTo;
+using SpellsRefresh.ElectroSpell;
 #endregion
 /// <summary>
 /// Do a dash jump forward
@@ -10,19 +11,52 @@ using XavHelpTo;
 public class SuperDashSpell : Spell
 {
     #region Variable
-
+    private RefreshController refresh;
+    [Header("Super Dash Spell")]
+    public Vector3 movement = new Vector3(0,10,5);
     #endregion
     #region Event
-
+    protected override void Start()
+    {
+        base.Start();
+        this.Component(out refresh);
+    }
     #endregion
     #region Method
+    /// <summary>
+    /// DO the dash without moving to any side
+    /// 
+    /// </summary>
+    private IEnumerator Dashing(Body body)
+    {
+        //Move the body
+
+        //while (true)
+        //{
+        yield return new WaitForEndOfFrame();
+        //}
+
+
+
+    }
     /// <summary>
     /// Do the heal cast
     /// </summary>
     public override void Cast(Body body)
     {
         if (!CanCast()) return; // 🛡
-        "CAST!".Print("blue");
+
+        //Moves
+        body.controller.Move(movement);
+        refresh.RefreshPlayParticle(Particle.ELECTRO);
+
+
+        //TODO Enviar estadod e paralizis al arma
+        //TODO HAcer daño x2 Y quitarlo trás hacer el ataque
+
+
+        //Start the dash
+        StartCoroutine(Dashing(body));
     }
     #endregion
 }
