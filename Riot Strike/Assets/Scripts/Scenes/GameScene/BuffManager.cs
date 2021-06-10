60a7ad587dbbf453f6425a1b12d54ab35e11a815
kingdox <arpaxavier@gmail.com>
2021-06-10 03:09:50 +0200

    no message

---
+++ Riot Strike/Assets/Scripts/Scenes/GameScene/BuffManager.cs
@@ -0,0 +1,68 @@
+﻿#region Access
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XavHelpTo;
+using XavHelpTo.Know;
+using XavHelpTo.Set;
+using XavHelpTo.Get;
+# endregion
+/// <summary>
+/// Manager to set in game the information of the prefabs Buffs
+/// </summary>
+public class BuffManager : MonoBehaviour
+{
+
+    #region Start
+    private Transform[][] jagged_buffs; // matrix donde asignamos toda la información en cada punto de manera ordenada
+    [SerializeField] private Transform tr_parent_buffs;
+
+    #endregion
+    #region Event
+    private void Start(){
+        LoadTheInformation();
+
+
+
+
+    }
+    #endregion
+    #region Method
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void LoadTheInformation(){
+        "Cargando información".Print("green");
+        foreach (Transform[] tr_buff in jagged_buffs){
+            //console.log/()
+            //Map(tr_buff);
+
+        }
+    }
+
+    /// <summary>
+    /// Maps the ui
+    /// </summary>
+    public void Map(Transform toMap)
+    {
+        //TODO en map te permitira revisar cadad uno por e
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Generate()
+    {
+
+    }
+
+    #endregion
+}
+
+/**
+ * 
+ * 
+ * 
+ */
